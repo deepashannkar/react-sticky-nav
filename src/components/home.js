493cd2ef175f994501650cbb2280259fad29c31b
deepashannkar <deepashannkar@gmail.com>
2022-07-06 21:26:02 +0530

    Initial commit

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SideNav from './sidenav';
+
+const Home = () => {
+    return (
+        <div>
+            <p>Welcome to home!</p>
+            <SideNav />
+        </div>
+    )
+}
+
+export default Home
